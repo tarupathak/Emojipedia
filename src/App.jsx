ef88d30feeab8d1e83ce28b2d0f99e54424bd7c5
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-06-10 22:48:35 +0530

    Made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
 import React from "react";
-import Entry from "./components/entry";
-import emojis from './components/emojis'
-
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      description={emojiTerm.meaning}
-    />
-  );
-}
+import emojipedia from "./components/emojipedia";
 
 function App() {
   return (
@@ -19,7 +7,21 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojis.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((item) => {
+          return (
+            <div className="term">
+              <dt>
+                <span className="emoji" role="img" aria-label="Tense Biceps">
+                  {item.emoji}
+                </span>
+                <span>{item.name}</span>
+              </dt>
+              <dd>{item.meaning}</dd>
+            </div>
+          );
+        })}
+      </dl>
     </div>
   );
 }
